357610f23364aeb8b9cad4d7383d86264544b8ab
Swiftz007 <cxllme14zx@gmail.com>
2026-04-22 04:52:29 +0700

    Update testreaper.lua

--- testreaper.lua
+++ testreaper.lua
@@ -17,7 +17,7 @@ local Camera = workspace.CurrentCamera
 --=========================
 local Window = Fluent:CreateWindow({
 Title = "Reaper Hub",
-SubTitle = "Premium X",
+SubTitle = "Beta 1.5",
 TabWidth = 160,
 Size = UDim2.fromOffset(520, 360),
 Acrylic = true,
