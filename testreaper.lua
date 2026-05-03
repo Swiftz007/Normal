85c896fa4ed555a11a4dfae5531dc90bce73399f
Swiftz007 <cxllme14zx@gmail.com>
2026-05-03 20:06:07 +0700

    Update Reaper Hub subtitle to 'lib Beta 9.7'

--- testreaper.lua
+++ testreaper.lua
@@ -17,7 +17,7 @@ local Camera = workspace.CurrentCamera
 --=========================
 local Window = Fluent:CreateWindow({
 Title = "Reaper Hub",
-SubTitle = "lib Beta 9.6",
+SubTitle = "lib Beta 9.7",
 TabWidth = 160,
 Size = UDim2.fromOffset(520, 360),
 Theme = "Dark",
