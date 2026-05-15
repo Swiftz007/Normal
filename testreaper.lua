5a1a2b7c6f3dd16a007b3f6e22147159725af4b6
Swiftz007 <cxllme14zx@gmail.com>
2026-05-16 01:59:53 +0700

    Update testreaper.lua

--- testreaper.lua
+++ testreaper.lua
@@ -4,7 +4,8 @@ task.wait("1")
 
 -- Lib Hwid 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Swiftz007/Libwtf/refs/heads/main/HwidSystem.lua"))()
-
+return
+end
 
 local Fluent = loadstring(game:HttpGet("https://raw.githubusercontent.com/Swiftz007/Advanced/refs/heads/main/main.lua"))()
 local SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/SaveManager.lua"))()
@@ -25,7 +26,7 @@ local Camera = workspace.CurrentCamera
 --=========================
 local Window = Fluent:CreateWindow({
 Title = "Reaper Hub",
-SubTitle = "lib Beta 18.8",
+SubTitle = "lib Beta 18.9",
 TabWidth = 160,
 Size = UDim2.fromOffset(520, 360),
 Theme = "Reaper",
