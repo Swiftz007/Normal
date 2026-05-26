ba6843ce1cddbc6d06dee77e674ae28615115e62
Swiftz007 <cxllme14zx@gmail.com>
2026-05-26 13:14:36 +0700

    Update testreaper.lua

--- testreaper.lua
+++ testreaper.lua
@@ -1,6 +1,6 @@
 -- Lib Load Screen Reaper Hub 3
 local Load = loadstring(game:HttpGet("https://raw.githubusercontent.com/Swiftz007/Libwtf/refs/heads/main/LoadLib.lua"))() 
-local hwid = loadstring(game:HttpGet("https://raw.githubusercontent.com/Swiftz007/Libwtf/refs/heads/main/HwidSystem.lua"))()
+
 
 local Fluent = loadstring(game:HttpGet("https://raw.githubusercontent.com/Swiftz007/Advanced/refs/heads/main/main.lua"))()
 local SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/SaveManager.lua"))()
