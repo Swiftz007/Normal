dd416ab89b286b25da2a19e9b84182e4f5467466
Swiftz007 <cxllme14zx@gmail.com>
2026-05-29 08:38:08 +0700

    Update testreaper.lua

--- testreaper.lua
+++ testreaper.lua
@@ -1,6 +1,10 @@
--- Lib all
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Swiftz007/Libwtf/refs/heads/main/Ui.lua"))() 
+-- Lib Load Screen Reaper Hub 10
+local Load = loadstring(game:HttpGet("https://raw.githubusercontent.com/Swiftz007/Libwtf/refs/heads/main/LoadLib.lua"))() 
+local hwid = loadstring(game:HttpGet("https://raw.githubusercontent.com/Swiftz007/Libwtf/refs/heads/main/HwidSystem.lua"))()
 
+local Fluent = loadstring(game:HttpGet("https://raw.githubusercontent.com/Swiftz007/Advanced/refs/heads/main/main.lua"))()
+local SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/Swiftz007/Advanced/refs/heads/main/SaveManager.lua"))()
+local InterfaceManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/Swiftz007/Advanced/refs/heads/main/InterfaceManager.lua"))()
 --=========================
 -- 🔥 SERVICES
 --=========================
