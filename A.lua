24b8b1b16cde5cf68aa3be93545ea8ba0d0b9178
Swiftz007 <cxllme14zx@gmail.com>
2026-06-14 02:30:39 +0700

    Create A.lua

---
+++ A.lua
@@ -0,0 +1,5 @@
+if _G.Script_Language == "Thai" then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Swiftz007/Normal/refs/heads/main/Thai.lua"))()
+else
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Swiftz007/Normal/refs/heads/main/testreaper.lua"))()
+end
