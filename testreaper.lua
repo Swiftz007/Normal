63bcc1d169f947bbe7da5ecb8b33405847a5c614
Swiftz007 <cxllme14zx@gmail.com>
2026-06-14 02:11:38 +0700

    Update script loading for Thai language

--- testreaper.lua
+++ testreaper.lua
@@ -1,6 +1,6 @@
 if _G.Script_Language == "Thai" then
     -- โหลดสคริปต์เฉพาะตอนที่เป็น Thai เท่านั้น
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/test/test/refs/heads/main/test"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Swiftz007/Normal/refs/heads/main/Thai.lua"))()
 else
 
 --=========================
