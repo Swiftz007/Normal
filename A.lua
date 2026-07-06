29a76a196007aa5e50efb37072af8a236cd894b2
Swiftz007 <cxllme14zx@gmail.com>
2026-07-06 23:15:52 +0700

    Comment out Thai script loading logic
    Comment out the conditional loading of Thai script.

--- A.lua
+++ A.lua
@@ -1,5 +1,5 @@
-if _G.Script_Language == "Thai" then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Swiftz007/Normal/refs/heads/main/Thai.lua"))()
-else
+--if _G.Script_Language == "Thai" then
+    --loadstring(game:HttpGet("https://raw.githubusercontent.com/Swiftz007/Normal/refs/heads/main/Thai.lua"))()
+--else
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Swiftz007/Normal/refs/heads/main/testreaper.lua"))()
-end
+--end
