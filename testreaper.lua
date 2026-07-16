7bc30374431ce8df10346f9d02d84709c6ae8509
Swiftz007 <cxllme14zx@gmail.com>
2026-07-16 16:12:18 +0700

    Update testreaper.lua

--- testreaper.lua
+++ testreaper.lua
@@ -1,5 +1,5 @@
 --=========================
--- 🔥 Lib Load Screen Reaper Hub 66
+-- 🔥 Lib Load Screen Reaper Hub 67
 --=========================
 local Load = loadstring(game:HttpGet("https://raw.githubusercontent.com/Swiftz007/Libwtf/refs/heads/main/libload2.lua"))() 
 local hwid = loadstring(game:HttpGet("https://raw.githubusercontent.com/Swiftz007/Libwtf/refs/heads/main/HwidSystem.lua"))()
@@ -599,8 +599,6 @@ Tabs.Player:AddSlider("SpinSpeed", {
 })
 
 -- ESP Chams🔥
-local Players = game:GetService("Players")
-local RunService = game:GetService("RunService")
 local LocalPlayer = Players.LocalPlayer
 
 local ChamsCache = {} -- ตะกร้าเก็บ Highlight เพื่อลดภาระการหา (Optimization)
